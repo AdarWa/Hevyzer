0268857611b6796015d78ca4ff432f9a6267fb83
AdarWa <adar.wasserman@gmail.com>
2025-08-24 17:37:54 +0300

    Add reports model

--- server.py
+++ server.py
@@ -7,16 +7,18 @@ import os
 from tasks.background import scheduler
 import tasks.strava_task as strava_task
 
-
+app = None
 
 STRAVA_CLIENT_ID = os.getenv("STRAVA_CLIENT_ID", "")
 STRAVA_CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET", "")
 STRAVA_REDIRECT_URI = os.getenv("STRAVA_REDIRECT_URI", "")
 CONFIG_PATH =str( os.getenv("CONFIG_PATH"))
+REPORTS_PATH =str( os.getenv("REPORTS_PATH"))
 EXTERNAL_DOMAIN = os.getenv("EXTERNAL_DOMAIN", "")
 
 
 def run():
+    global app
     app = Flask(__name__)
     app.config['SERVER_NAME'] = EXTERNAL_DOMAIN 
 
@@ -28,7 +30,10 @@ def run():
     app.app_context().push()
     if not Path(CONFIG_PATH).exists():
         models.Config.get_default_config().save(CONFIG_PATH)
+    if not Path(REPORTS_PATH).exists():
+        models.Config.get_default_config().save(REPORTS_PATH)
 
+    models.reports = models.Reports.load(REPORTS_PATH)
     models.config = models.Config.load(CONFIG_PATH)
     strava_task.add_job()
     scheduler.start()
