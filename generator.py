571b67b00d098aee0e877a114de407267cc0b582
AdarWa <adar.wasserman@gmail.com>
2025-08-24 17:37:26 +0300

    Send report using mailer

--- generator.py
+++ generator.py
@@ -3,13 +3,12 @@ from os import getenv
 from auth.models import Report
 from hevy_parser import parse_workout, report_to_text
 from openai import OpenAI
-client = OpenAI(api_key=getenv("LLM_API_KEY"))
+import markdown
+from mailer import Mailer
+import auth.models as models
+client = OpenAI(api_key=getenv("LLM_API_KEY", "ollama"), base_url="http://localhost:11434/v1")
 
-def analyze_report(report: Report, model: str = "gpt-4") -> str:
-    """
-    Converts a Report to text and asks the OpenAI API to analyze it.
-    Returns the LLM response as a string.
-    """
+def analyze_report(report: Report, model: str = "deepseek-coder-v2:16b") -> str: # mistral:7b-instruct-q4_K_M for testing
     workout_text = report_to_text(report)
 
     prompt = f"""
@@ -18,19 +17,30 @@ You are a professional fitness coach. Analyze the following workout report and p
 - Highlight exercises with poor progressive overload
 - Give tips on reps, sets, or weight adjustments
 
+The trainee measures are:
+Bodyweight: 47kg
+Age: 16y 
+
 Workout Report:
 {workout_text}
 
-Please provide a clear, structured analysis.
+Please provide a clear, structured analysis, use markdown for clear structure.
 """
 
-    response = client.responses.create(
-        model="deepseek-coder-v2:16b",
-        input="Write a one-sentence bedtime story about a unicorn."
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            # {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt}
+        ]
     )
 
-    return response.output_text
+    return response.choices[0].message.content or "Empty Response."
 
+def send_report(report: Report, model: str = "mistral:7b-instruct-q4_K_M"):
+    analyzed = analyze_report(report, model)
+    final = "# Hevyzer Report for " + report.name + "\n\n" + analyzed
+    Mailer().send("Hevyzer Report", markdown.markdown(final), models.config.emails, html=True)
 
 if __name__ == "__main__":
     workout_text = """Triceps Rope Pushdown
